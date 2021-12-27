fcac09c16feb56dd925f007711ec6b5d631e3804
Vladimir <vatsup@mail.ru>
2021-12-27 16:06:12 +0300

    lesson  2.9

---
+++ Scripts/js.js
@@ -0,0 +1,23 @@
+if (2+2==4) {
+ console.log('True');   
+}else{
+    console.log('False');
+}
+let num = 250;
+(num>40)? console.log('Tr'):console.log('Fl');
+switch(num) {
+    case 50:
+        console.log('num = 50');
+        break;
+    case 100:
+        console.log('num = 100');
+        break;
+    case 200:
+        console.log('num =  200');
+        break;
+    case 250:
+        console.log('num = 250');
+        break;
+    default:
+        console.log(`num = ${num}`);
+}
